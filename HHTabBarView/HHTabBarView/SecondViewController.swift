06637b0915b919b8ea13bc116646726e54b27731
Hemang Shah <hemangshah.in@gmail.com>
2017-07-25 16:42:20 +0530

    Updated UI for the HHTabBar and UINavigationBar Show/Hide

--- HHTabBarView/HHTabBarView/SecondViewController.swift
+++ HHTabBarView/HHTabBarView/SecondViewController.swift
@@ -10,29 +10,25 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet var btnShowHideTabBar: UIButton!
     var apd = UIApplication.shared.delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Refresh"
-        
-        btnShowHideTabBar.layer.cornerRadius = 12.0
-        btnShowHideTabBar.layer.masksToBounds = true
-        btnShowHideTabBar.layer.borderColor = UIColor.black.cgColor
-        btnShowHideTabBar.layer.borderWidth = 3.0
     }
     
-    @IBAction func actionShowHideTabBar(_ sender: UIButton) {
+    @IBAction func actionShowHideTabBar(_ sender: UISwitch) {
         
         if !apd.hhTabBarView.isHidden {
             apd.hideTabBar()
-            btnShowHideTabBar.setTitle("Show TabBar", for: .normal)
         } else {
             apd.showTabBar()
-            btnShowHideTabBar.setTitle("Hide TabBar", for: .normal)
         }
         
+    }
+    
+    @IBAction func actionShowHideNavigationBar(_ sender: UISwitch) {
+        self.navigationController?.isNavigationBarHidden = !sender.isOn
     }
 }
