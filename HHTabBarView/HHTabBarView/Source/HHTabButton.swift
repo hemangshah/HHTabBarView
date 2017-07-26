afc1f946ab07308c313f7ebc87f01768abd99934
Hemang Shah <hemangshah.in@gmail.com>
2017-07-26 12:05:54 +0530

    Code Refactoring

--- HHTabBarView/HHTabBarView/Source/HHTabButton.swift
+++ HHTabBarView/HHTabBarView/Source/HHTabButton.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-extension UIButton {
-    func setBackgroundColor(color: UIColor, forState: UIControlState) {
+public extension UIButton {
+   public func setBackgroundColor(color: UIColor, forState: UIControlState) {
         UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
         UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
         UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
@@ -28,33 +28,33 @@ public class HHTabButton: UIButton {
     required
     convenience public init(withTitle tabTitle:String, tabImage: UIImage, index:Int) {
         self.init(frame: CGRect.zero)
-        self.backgroundColor = UIColor.clear
-        self.setTitle(tabTitle, for: .normal)
-        self.setImage(tabImage, for: .normal)
-        self.titleEdgeInsets = .init(top: 0, left: 10, bottom: 0, right: 0)
-        self.setTitleColor(.black, for: .normal)
-        self.autoresizingMask = [.flexibleWidth, .flexibleLeftMargin, .flexibleRightMargin, .flexibleHeight]
-        self.tabIndex = index
+        setupButton(withTitle: tabTitle, tabImage: tabImage, index: index)
     }
     
     required
     convenience public init(tabImage: UIImage, index:Int) {
         self.init(frame: CGRect.zero)
-        self.backgroundColor = UIColor.clear
-        self.setImage(tabImage, for: .normal)
-        self.titleEdgeInsets = .init(top: 0, left: 10, bottom: 0, right: 0)
-        self.setTitleColor(.black, for: .normal)
-        self.autoresizingMask = [.flexibleWidth, .flexibleLeftMargin, .flexibleRightMargin, .flexibleHeight]
-        self.tabIndex = index
+        setupButton(withTitle: nil, tabImage: tabImage, index: index)
     }
     
     required
     convenience public init(withTitle tabTitle:String, index:Int) {
         self.init(frame: CGRect.zero)
+        setupButton(withTitle: tabTitle, tabImage: nil, index: index)
+    }
+    
+    fileprivate func setupButton(withTitle tabTitle: String?, tabImage: UIImage?, index: Int) {
         self.backgroundColor = UIColor.clear
-        self.setTitle(tabTitle, for: .normal)
-        self.titleEdgeInsets = .init(top: 0, left: 10, bottom: 0, right: 0)
-        self.setTitleColor(.black, for: .normal)
+        
+        if let title = tabTitle {
+            self.setTitle(title, for: .normal)
+            self.setTitleColor(.black, for: .normal)
+        }
+        
+        if let image = tabImage {
+            self.setImage(image, for: .normal)
+        }
+        
         self.autoresizingMask = [.flexibleWidth, .flexibleLeftMargin, .flexibleRightMargin, .flexibleHeight]
         self.tabIndex = index
     }
